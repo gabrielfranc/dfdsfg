676f37af0574a0f7027d9922b5c040939fb9963a
Gabriel França <115255675+gabrielfranc@users.noreply.github.com>
2023-10-09 12:01:01 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function TocarSomPom(){
+    document.querySelector("#som_tecla_pom").play();
+}
+
+
+
+function TocarSomA(){
+    document.querySelector("#som_tecla_A").play();
+}
+
+
+
+function TocarSomOof(){
+    document.querySelector("#som_tecla_off").play();
+}
+
+
+
+function TocarSomPipe(){
+    document.querySelector("#som_tecla_Pipe").play();
+}
+function TocarSomClap(){ 
+Document.querySelector("#som_tecla_Clap").play();
+}
+
